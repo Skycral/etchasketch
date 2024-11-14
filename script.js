1a6317aacaa148a073860d65dbf035f69dce050a
Skyborn <c.skyborn@hotmail.com>
2024-11-14 14:19:42 +0100

    feat!: add button to change size of grid
    Added functionality to be able for user to change size
    of the grid. Changed some logic in fn createGrid to
    adjust squares accordingly.

--- script.js
+++ script.js
@@ -4,18 +4,29 @@ const button = document.querySelector("button");
 let grid = 16;
 
 function createBoxes(number) {
-  for (let i = 0; i < grid * grid; i++) {
+  for (let i = 0; i < number * number; i++) {
     const div = document.createElement("div");
     div.setAttribute("class", "box");
+    div.style.width = `${100/number}%`;
+    div.style.height = `${100/number}%`;
     container.appendChild(div);
+    console.log(div.style.width, div.style.height);
   }
 }
 
+createBoxes(grid);
 
 
-
-
-
+button.addEventListener("click", () => {
+  let number = prompt("How large should the grid become?");
+  if(number > 100) {
+    alert("This grid is too big, please enter a smaller number.")
+  } else {
+    container.replaceChildren();
+    console.log(number);
+    createBoxes(number);
+  }
+})
 
 
 
