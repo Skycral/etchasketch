000d7012adf372d328c8be83d0d6073ba35c83da
Skyborn <c.skyborn@hotmail.com>
2024-11-14 16:39:55 +0100

    feat: add hover effect when hovering boxes
    Added hover effect which increases opacity and changes
    background color each time. Performance was a bit
    decreased.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
 const button = document.querySelector("button");
 
-let grid = 16;
+let number = 16;
 
 function createBoxes(number) {
   for (let i = 0; i < number * number; i++) {
@@ -10,12 +10,10 @@ function createBoxes(number) {
     div.style.width = `${100/number}%`;
     div.style.height = `${100/number}%`;
     container.appendChild(div);
-    console.log(div.style.width, div.style.height);
   }
 }
 
-createBoxes(grid);
-
+createBoxes(number);
 
 button.addEventListener("click", () => {
   let number = prompt("How large should the grid become?");
@@ -23,10 +21,40 @@ button.addEventListener("click", () => {
     alert("This grid is too big, please enter a smaller number.")
   } else {
     container.replaceChildren();
-    console.log(number);
     createBoxes(number);
+    changeColor();
   }
 })
+
+// create randomize for background color on boxes
+function randomColor() {
+  return Math.floor(Math.random()*16777215).toString(16);
+}
+
+
+// fill boxes with random color when hover over them
+
+const boxes = container.getElementsByTagName("div");
+boxes[0].style.backgroundColor = `#${randomColor()}`;
+
+console.log(boxes.length);
+
+
+function changeColor() {
+
+  for (let i = 0; i < boxes.length; i++) {
+    let opa = 0;
+
+    boxes[i].addEventListener("mouseover", () => {
+      opa += 0.1;
+      boxes[i].style.backgroundColor = `#${randomColor()}`;
+      boxes[i].style.opacity = opa;
+    });
+  }
+}
+
+changeColor();
+
 
 
 
